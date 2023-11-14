72fb78d6f86be8a212abb88712c4682ef107d07a
Soumi-Paul-dev-99 <soumi.paul.dev99@gmail.com>
2023-11-14 13:01:32 +0530

    second commit

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
 import express from "express";
 import colors from "colors";
-import dotenv from"dotenv";
+import dotenv from "dotenv";
 dotenv.config();
 import database from "./config/Db.js";
 import morgan from "morgan";
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+
+}));
 
 database();
-const app = express()
-const Port = process.env.PORT || 5000
 
+const Port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
 app.use(morgan("dev"));
 
 ///routes
-app.use("/api/v1/user",userRoutes)
+app.use("/api/v1/user", userRoutes);
 
-app.get("/",(req,res)=>{
-    res.status(201).json({
-        message:"welcome to ecommerce app"
-    })
+app.get("/", (req, res) => {
+  res.status(201).json({
+    message: "welcome to ecommerce app",
+  });
 });
 
-app.listen(Port , ()=>{
-    console.log(`server running on the port http://localhost:${Port}`.bgCyan.black)
+app.listen(Port, () => {
+  console.log(
+    `server running on the port http://localhost:${Port}`.bgCyan.black
+  );
 });
